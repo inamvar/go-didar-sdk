fcebe8eaf00dc20815922d2e67bbe9d2f4348b48
Iman Namvar <iman.namvar@gmail.com>
2022-10-11 12:44:21 +0330

    fix contact feilds

--- base.go
+++ base.go
@@ -1,23 +1,20 @@
 package didar
 
-type ContactField struct {
-	Key   string `json:"Key"`
-	Value any    `json:"Value"`
-}
+type ContactField map[string]interface{}
 
 type Contact struct {
-	Id           string         `json:"Id,omitempty"`
-	FirstName    string         `json:"FirstName"`
-	LastName     string         `json:"LastName"`
-	Email        string         `json:"Email,omitempty"`
-	MobilePhone  string         `json:"MobilePhone,omitempty"`
-	WorkPhone    string         `json:"WorkPhone,omitempty"`
-	CompanyName  string         `json:"CompanyName,omitempty"`
-	CompanyId    string         `json:"CompanyId,omitempty"`
-	Fields       []ContactField `json:"Fields,omitempty"`
-	SegmentIds   []string       `json:"SegmentIds,omitempty"`
-	OwnerId      string         `json:"OwnerId,omitempty"`
-	CustomerCode string         `json:"CusomterCode,omitempty"`
+	Id           string       `json:"Id,omitempty"`
+	FirstName    string       `json:"FirstName"`
+	LastName     string       `json:"LastName"`
+	Email        string       `json:"Email,omitempty"`
+	MobilePhone  string       `json:"MobilePhone,omitempty"`
+	WorkPhone    string       `json:"WorkPhone,omitempty"`
+	CompanyName  string       `json:"CompanyName,omitempty"`
+	CompanyId    string       `json:"CompanyId,omitempty"`
+	Fields       ContactField `json:"Fields,omitempty"`
+	SegmentIds   []string     `json:"SegmentIds,omitempty"`
+	OwnerId      string       `json:"OwnerId,omitempty"`
+	CustomerCode string       `json:"CusomterCode,omitempty"`
 }
 
 type ContactRequest struct {
